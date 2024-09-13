3129c2a21456f64326f6d4627d93c8f93a570dc4
Johannes Hertrich <j.hertrich@ucl.ac.uk>
2024-09-13 16:00:52 +0100

    fix vectorization

--- nfft.py
+++ nfft.py
@@ -28,9 +28,9 @@ def transposed_sparse_convolution(x,f,n,m,phi_conj,device,complex_type):
     inds=(torch.ceil(n*x).long()-m)[None]+l
     increments=phi_conj(x[None,:,:]-inds.float()/n)*f
     g_linear=torch.zeros((x.shape[0]*n,),device=device,dtype=complex_type)
-    inds=(inds+n//2)+x.shape[1]*torch.arange(0,x.shape[0],device=device,dtype=torch.long)[:,None] # +n//2 weil index shift von -n/2 bis n/2-1 zu 0 bis n-1, anderer Term für lineare Indizes
-    g_linear.index_put_((inds.view(-1),),increments.view(-1),accumulate=True)
-    return g_linear.view(x.shape[0],-1)
+    inds=(inds+n//2)+n*torch.arange(0,x.shape[0],device=device,dtype=torch.long)[None,:,None] # +n//2 weil index shift von -n/2 bis n/2-1 zu 0 bis n-1, anderer Term für lineare Indizes
+    g_linear.index_put_((inds.reshape(-1),),increments.reshape(-1),accumulate=True)
+    return g_linear.view(x.shape[0],n)
 
 def adjoint_nfft(x,f,N,n,m,phi_conj,phi_hat,device,complex_type):
     # x ist zweidimensional: erst batch-dimension, dann die Stützpunkte
@@ -42,9 +42,9 @@ def adjoint_nfft(x,f,N,n,m,phi_conj,phi_hat,device,complex_type):
     # phi_hat fängt mit negativen indizes an
     cut=(n-N)//2
     g=transposed_sparse_convolution(x,f,n,m,phi_conj,device,complex_type)
-    g=torch.fft.ifftshift(g)
+    g=torch.fft.ifftshift(g,[-1])
     g_hat=torch.fft.ifft(g,norm="forward")
-    g_hat=torch.fft.fftshift(g_hat)[:,cut:-cut]
+    g_hat=torch.fft.fftshift(g_hat,[-1])[:,cut:-cut]
     f_hat=g_hat/phi_hat
     # f_hat fängt mit negativen indizes an
     return f_hat
@@ -59,7 +59,7 @@ def sparse_convolution(x,g,n,m,M,phi,device):
     l=torch.arange(0,2*m,device=device,dtype=torch.long).view(2*m,1,1)
     inds=(torch.ceil(n*x).long()-m)[None,:,:]+l
     increments=phi(x[None,:,:]-inds/n).to(torch.complex64)
-    inds=inds+n//2+x.shape[1]*torch.arange(0,x.shape[0],device=device,dtype=torch.long)[:,None] # +n//2 weil index shift von -n/2 bis n/2-1 zu 0 bis n-1, anderer Term für lineare Indizes
+    inds=inds+n//2+n*torch.arange(0,x.shape[0],device=device,dtype=torch.long)[:,None] # +n//2 weil index shift von -n/2 bis n/2-1 zu 0 bis n-1, anderer Term für lineare Indizes
     g_l=g.view(-1)[inds].view(increments.shape)
     increments*=g_l
     f=torch.sum(increments,0)
@@ -77,8 +77,8 @@ def forward_nfft(x,f_hat,N,n,m,phi,phi_hat,device):
     # f_hat fängt mit negativen indizes an
     g_hat=f_hat/phi_hat
     pad=torch.zeros((x.shape[0],(n-N)//2),device=device)
-    g_hat=torch.fft.fftshift(torch.cat((pad,g_hat,pad),1))
-    g=torch.fft.ifftshift(torch.fft.fft(g_hat,norm="backward")) # damit g wieder auf [-1/2,1/2) lebt
+    g_hat=torch.fft.fftshift(torch.cat((pad,g_hat,pad),1),[-1])
+    g=torch.fft.ifftshift(torch.fft.fft(g_hat,norm="backward"),[-1]) # damit g wieder auf [-1/2,1/2) lebt
     f=sparse_convolution(x,g,n,m,x.shape[1],phi,device)
     # f hat die gleiche Größe wie x
     return f
--- test.py
+++ test.py
@@ -3,15 +3,16 @@ import torch
 import torchkbnufft as tkbn
 import time
 
-device='cuda'
+import torch_nfft as tn
+
+device='cpu'
 double_precision=False
 float_type=torch.float64 if double_precision else torch.float32
 complex_type=torch.complex128 if double_precision else torch.complex64
 
-N=2*20
+N=100#2**10
 J=20000
-k=.3*(torch.rand((J,),device=device,dtype=float_type)-.5)
-k=k[None,:]
+k=.3*(torch.rand((2,J,),device=device,dtype=float_type)-.5)
 m=8
 sigma=2
 
@@ -34,7 +35,7 @@ f=torch.randn(k.shape,dtype=complex_type,device=device)
 fHat=nfft.adjoint(k,f)
 
 # comparison with NDFT
-fHat_dft=ndft_adjoint(k.squeeze(),f.squeeze(),ft_grid)
+fHat_dft=torch.stack([ndft_adjoint(k[i],f[i],ft_grid) for i in range(k.shape[0])],0)
 
 # relative error
 print("Relativer Fehler", torch.sqrt(torch.sum(torch.abs(fHat-fHat_dft)**2)/torch.sum(torch.abs(fHat_dft)**2)))
@@ -51,23 +52,27 @@ fHat=torch.randn((k.shape[0],N),dtype=complex_type,device=device)
 f=nfft(k,fHat)
 
 # comparison with NDFT
-f_dft=ndft_forward(k.squeeze(),fHat.squeeze(),ft_grid)
+f_dft=torch.stack([ndft_forward(k[i],fHat[i],ft_grid) for i in range(k.shape[0])],0)
 
 # relative error
 print("Relativer Fehler", torch.sqrt(torch.sum(torch.abs(f-f_dft)**2)/torch.sum(torch.abs(f_dft)**2)))
 
-
+exit()
 ##############################################
 ###### Compare with torchkbnufft... ##########
 ##############################################
 
 
-J=100000
-k=.3*(torch.rand((2,J,),device=device,dtype=float_type)-.5)
+J=300000
+k=.3*(torch.rand((1,J,),device=device,dtype=float_type)-.5)
 #k=k[None,:]
 
 # test data
 f=torch.randn(k.shape,dtype=complex_type,device=device)
+
+# ground truth via NDFT
+#fHat_dft=ndft_adjoint(k.squeeze(),f.squeeze(),ft_grid)
+
 
 torch.cuda.synchronize()
 tic=time.time()
@@ -76,11 +81,24 @@ fHat=nfft.adjoint(k,f)
 torch.cuda.synchronize()
 toc=time.time()-tic
 print(toc)
-exit()
 
-k_kbnufft=k*torch.pi
+torch.cuda.synchronize()
+tic=time.time()
+fHat_torch_nfft=tn.nfft_adjoint(f.flatten(), k.flatten()[:,None], bandwidth=N, cutoff=m)
+torch.cuda.synchronize()
+toc=time.time()-tic
+print(toc)
 
-adjnufft_ob = tkbn.KbNufftAdjoint(im_size=(N,)).to(device)
+import matplotlib.pyplot as plt
+#plt.plot(ft_grid.detach().cpu().numpy(),torch.abs(fHat_dft).detach().cpu().numpy().squeeze(),"k1")
+plt.plot(ft_grid.detach().cpu().numpy(),torch.abs(fHat).detach().cpu().numpy().squeeze(),"rx")
+plt.plot(ft_grid.detach().cpu().numpy(),torch.abs(fHat_torch_nfft).detach().cpu().numpy().squeeze(),"b+")
+plt.show()
+
+"""
+k_kbnufft=2*k
+
+adjnufft_ob = tkbn.KbNufftAdjoint(im_size=(N,),grid_size=(2*N,),numpoints=8,device=device).to(device)
 
 torch.cuda.synchronize()
 tic1=time.time()
@@ -98,8 +116,10 @@ print(toc,toc1)
 
 print(torch.abs(fHat_kb).sum()/torch.abs(fHat).sum())
 
+
 import matplotlib.pyplot as plt
+plt.plot(ft_grid.detach().cpu().numpy(),torch.abs(fHat_dft).detach().cpu().numpy().squeeze(),"k1")
 plt.plot(ft_grid.detach().cpu().numpy(),torch.abs(fHat).detach().cpu().numpy().squeeze(),"rx")
 plt.plot(ft_grid.detach().cpu().numpy(),torch.abs(fHat_kb).detach().cpu().numpy().squeeze(),"b+")
 plt.show()
-
+"""
